513de1cb2058a9b854e482c6539719fb216d281a
nikola-burazor <nikola.burazor@gmail.com>
2016-09-21 12:47:28 +0000

    parse request header as required

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.get('/api/whoami', function (req, res) {
-	jsonRes = {"ipaddress":null,"language":null,"software":null};
 
-
-
+	var ip = req.headers['x-forwarded-for'];
+	var lang = req.headers['accept-language'].split(',')[0];
+	var software = req.headers['user-agent'].split('(')[1].split(')')[0];
+	//console.log(req.headers);
+	jsonRes = {"ipaddress":ip,"language":lang,"software":software};
 	res.end(JSON.stringify(jsonRes));	
 });
 app.get('/',function(req,res){
